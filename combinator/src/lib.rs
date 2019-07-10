97bd655085413571099bae6207efb2b55b3f9277
Julian Zhang <zhangchunzhong@gmail.com>
2019-07-10 22:39:23 +0800

    note backup at 09:38 @home

---
+++ combinator/src/lib.rs
@@ -0,0 +1,16 @@
+
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
